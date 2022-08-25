306c441581a50c83b96b34ef206c62e8210f91d1
Sebastian Romano <70913476+sebastianromano@users.noreply.github.com>
2022-08-26 01:28:11 +0200

    added more assignments

--- 2022-05-20 Exam practise/One/AB.java
+++ 2022-05-20 Exam practise/One/AB.java
@@ -1,5 +1,13 @@
 package One;
 
 public class AB {
-    
+    public static void main(String[] args) {
+        // 1.A
+        // Which pair of functions satisfy f(N) ~ g(N)?
+        // A: f(N) = 2N and g(N) = N + √N
+        System.out.println(Math.sqrt(10));
+
+        // 1.B
+        // Which pair of functions satisfy f(N) ~ O(g(N))?
+    }
 }
--- 2022-05-20 Exam practise/One/CDEFG.java
+++ 2022-05-20 Exam practise/One/CDEFG.java
@@ -6,10 +6,47 @@ package One;
  */
 
 public class CDEFG {
+    private static int[] A;
+
     public static void main(String[] args) {
-        int N = 100;
+        int N = 20;
+
+        // 2.C
+        System.out.println("\n2.C");
         for (int i = 1; i < N; i = i * 2) {
-            System.out.println("*");
+            System.out.print("*");
+        }
+
+        // 2.D
+        System.out.println("\n2.D");
+        for (int i = 0; i < N; i = i + 1) {
+            for (int j = i; j > 0; j = j - 2) {
+                System.out.print("*");
+            }
+        }
+
+        // 2.E
+        System.out.println("\n2.E");
+        for (int i = 0; i < N; i = i + 1) {
+            if (i < 5) {
+                for (int j = 0; j < N; j = j + 1) {
+                    // A[i] = A[i] + A[j] + i;
+                }
+            }
+        }
+
+        // 2.G
+        System.out.println("\n2.G");
+        System.out.println(r(N));
+    }
+
+    // 2.F
+    // System.out.println("\n2.F");
+    static int r(int N) {
+        if (N > 0) {
+            return r(N - 1) + r(N - 1) + N;
+        } else {
+            return 2;
         }
     }
 }
