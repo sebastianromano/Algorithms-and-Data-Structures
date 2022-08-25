6a054bf4dc9dfa73a329203b66ae48f38677bb27
Sebastian Romano <70913476+sebastianromano@users.noreply.github.com>
2022-08-25 22:13:51 +0200

    add new class and package

---
+++ 2022-05-20 Exam practise/One/CDEFG.java
@@ -0,0 +1,15 @@
+package One;
+
+/*
+ * The following code was featured in the *IT University of Copenhagen *Algorithms and Data Structures** exam on 2019-05-20
+ * The code was shared online by the teachers for practise
+ */
+
+public class CDEFG {
+    public static void main(String[] args) {
+        int N = 100;
+        for (int i = 1; i < N; i = i * 2) {
+            System.out.println("*");
+        }
+    }
+}
