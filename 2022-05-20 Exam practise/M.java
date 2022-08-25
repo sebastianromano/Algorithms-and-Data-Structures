99bb77317bd26dc16c39157b215d530cbb854991
Sebastian Romano <70913476+sebastianromano@users.noreply.github.com>
2022-08-25 14:47:20 +0200

    Preliminary method reportmin()

--- 2022-05-20 Exam practise/M.java
+++ 2022-05-20 Exam practise/M.java
@@ -1,11 +1,22 @@
 /*
- * The following code was featured in the IT-University exam (2019-05-20) and was shared by the teachers for practise. 
+ * The following code was featured in the *IT University of Copenhagen *Algorithms and Data Structures** exam on 2019-05-20 
+ * The code was shared online by the teachers for practise
+ * 
+ * Added code by @sebastianromano reflects following assignments: 
+ *  1. Write a method reportmin() that returns to smallest inserted element. 
+ *      If the data structure is empty, you may return anything or your program may fail. 
+ *      Do not change the insert() method.
+ *  2. Modify the data structure so that the space usage is proportional to the number of different items inserted. 
+ *      You may not change the worst-case running time of any of the methods. 
+ *      (But you may change their amortised running time.)
+ *  3. ...
  */
 
 public class M {
     int[] L = { 0 };
     int N = 0;
-    int max = Integer.MIN_VALUE;
+    int max = Integer.MIN_VALUE; // max is used to find the maximum value in the array
+    int min = Integer.MAX_VALUE; // min is used to find the minimum value in the array (@sebastianromano)
 
     void insert(int item) {
         if (L.length == N)
@@ -35,8 +46,17 @@ public class M {
         return max;
     }
 
+    int reportmin() { // method that returns the smallest inserted element (@sebastianromano)
+        for (int i = 0; i < N; i++)
+            if (L[i] < min)
+                min = L[i];
+        return min;
+    }
+
     public static void main(String[] args) {
         M newM = new M();
+        System.out.println(newM.reportmax());
+        System.out.println(newM.reportmin());
 
         System.out.println("2.b"); // exam question 2.b
         newM.insert(3);
@@ -47,5 +67,7 @@ public class M {
         newM.insert(4);
         System.out.println(newM.contains(3));
         System.out.println();
+        System.out.println(newM.reportmin());
+        System.out.println(newM.reportmax());
     }
 }
